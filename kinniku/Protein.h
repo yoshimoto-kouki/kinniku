f93e36ca84ae94c8282c802d8e97b0d0693a5380
haru-shinobu <r.t0desu@gmail.com>
2019-09-04 13:22:15 +0900

    haru-shinobu commit

---
+++ kinniku/Protein.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef __IGAMEOBJECT_H__
+#include "IGameObject.h"
+#endif
+
+//struct ID2D1RenderTarget;
+struct ID2D1Bitmap;
+class CStage;
+
+class CProtein : public IGameObject
+{
+public:
+	CProtein(float x, float y);
+	~CProtein();
+
+	virtual bool move() override;
+	virtual void draw(ID2D1RenderTarget *pRenderTarget) override;
+	virtual bool collide(float x, float y, float w, float h) override;
+	virtual bool collide(IGameObject *pObj) override;
+	virtual void hit(float amount) override;
+	static void Restore(CStage *pStage, ID2D1RenderTarget *pRT);
+	static void Finalize();
+	virtual bool make()override;
+
+protected:
+	//static ID2D1RenderTarget *pRenderTarget;
+	static ID2D1Bitmap *m_pTexture;
+	static CStage * m_pParent;
+	BOOL  m_bDamage;
+	float m_fX, m_fY;
+	float m_fVX, m_fVY;
+};
